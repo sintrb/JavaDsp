69d705c6e78121ebf771145a606c4c8870a914dd
RobinTang <sintrb@gmail.com>
2014-05-10 12:07:12 +0800

    版本信息

---
+++ src/com/sin/java/dsp/JavaDsp.java
@@ -0,0 +1,13 @@
+package com.sin.java.dsp;
+
+
+/**
+ * 数字信号处理(DSP)方面的Java封装，包含常用的一些处理方法，如滤波、信号变换等等。
+ * 
+ * @author RobinTang
+ * @version 1.0
+ * @see https://github.com/sintrb/JavaDsp/
+ */
+public class JavaDsp {
+	public static final String VERSION = "1.0";
+}
